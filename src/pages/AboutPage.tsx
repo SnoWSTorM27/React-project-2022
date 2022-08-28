095062baa25413b8aab7404893aea3372da8db29
SnoWSTorM27 <snowstorm27@ya.ru>
2022-08-28 12:08:10 +0700

    React test-project-2022 routes context modal props fakeAPI

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function AboutPage() {
+  return (
+    <div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed suscipit quis obcaecati laborum harum laudantium dolores nulla enim, explicabo placeat praesentium animi repellendus doloremque recusandae impedit, ipsum dignissimos error? Doloribus dolores, nemo fugit deleniti ullam animi, nam nostrum architecto quaerat, placeat tempore! Molestiae, aliquam exercitationem excepturi quis provident architecto animi.</div>
+  )
+}
